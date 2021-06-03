6757e6e6d5010996cebf272de7196f1cac685688
MierenManz <christiaanmens@gmail.com>
2021-06-03 15:34:35 +0200

    optimize

--- lexer/lib.rs
+++ lexer/lib.rs
@@ -5,19 +5,17 @@ use node_gen::*;
 use regex::Regex;
 use structures::*;
 
-pub fn generate_ast(mut markdown_string: String) -> AST {
+#[inline(always)]
+pub fn generate_ast(markdown_string: String) -> AST {
   let mut ast = AST { body: Vec::new() };
-
-  markdown_string = markdown_string.replace("\r\n", "\n");
-
   let chararray = markdown_string.chars();
   let array_length = markdown_string.len();
+  let s_list_regex = Regex::new("/^[0-9]\\.\\s/g").unwrap();
   let mut line = String::new();
   let mut i = 0;
   let mut iter = 0;
 
   for character in chararray {
-    let start = std::time::Instant::now();
     iter += 1;
 
     if character != '\n' {
@@ -25,39 +23,30 @@ pub fn generate_ast(mut markdown_string: String) -> AST {
     } else if character == '\n' || iter == array_length {
       line = String::from(line.trim());
       if line.starts_with("#") {
-        ast.body.push(header_node(&line));
-      } else if line.starts_with("- ")
-        || line.starts_with("* ")
-        || Regex::new("/^[0-9]\\.\\s/g").unwrap().is_match(&line)
-      {
-        let buff = list_node(&line);
-
-        if i > 0 && i < ast.body.len() {
+        if i - 1 < ast.body.len() {
           ast.body[i - 1].include_next_line = false;
-          ast.body.push(new_line());
-          ast.body.push(buff);
         }
+        ast.body.push(header_node(&line));
+      } else if line.starts_with("- ") || s_list_regex.is_match(&line) {
+        if i - 1 < ast.body.len() {
+          ast.body[i - 1].include_next_line = false;
+        }
+
+        ast.body.push(list_node(&line));
       } else if line != "" {
         ast.body.push(normal_line(&line));
-      }
-
-      if !ast.body[ast.body.len() - 1].include_next_line
-        || ast.body[ast.body.len() - 1].original_string != Some(String::new())
-      {
+      } else {
         ast.body.push(new_line());
       }
 
       line = String::new();
       i += 1;
-      let node = String::from(ast.body[ast.body.len() - 1].node_type);
-      if node != "NewLine" {
-        println!(
-          "time: {}, node: {:?}",
-          start.elapsed().as_micros(),
-          ast.body[ast.body.len() - 1].node_type
-        );
-      }
     }
+  }
+
+  {
+    let len = ast.body.len();
+    ast.body[len - 1].include_next_line = false;
   }
 
   return ast;
