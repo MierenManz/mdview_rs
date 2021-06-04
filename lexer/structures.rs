941e9974c7518dd80fd4252910d76f910ddf12a7
MierenManz <christiaanmens@gmail.com>
2021-06-04 09:39:19 +0200

    feat: leave debug in here for now for debugging

--- lexer/structures.rs
+++ lexer/structures.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum NodeType {
   Header,
   NewLine,
@@ -9,14 +9,14 @@ pub enum NodeType {
   SList,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct TextAttributes {
   pub image_or_link: bool,
   pub strike: bool,
   pub bold_or_italics: bool,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct NodeInfo {
   pub string: Option<String>,
   pub attributes: Option<TextAttributes>,
@@ -24,7 +24,7 @@ pub struct NodeInfo {
   pub sorted_list_number: Option<usize>,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Node {
   pub node_type: NodeType,
   pub node_info: NodeInfo,
@@ -32,7 +32,7 @@ pub struct Node {
   pub allow_merge: bool,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct AST {
   pub body: Vec<Node>,
 }
