3d65817232648627e162bf4142e56fefad18a7f7
mierenmanz <christiaanmens@gmail.com>
2021-06-17 22:27:31 +0200

    change web-view lib

--- src/window/lib.rs
+++ src/window/lib.rs
@@ -1,15 +1,15 @@
-use web_view::Content::Html;
-use web_view::WebViewBuilder;
+use webview::Content;
+use webview::WebView;
 
 pub fn create_window(text: &str, title: &str, debug: bool) {
-    WebViewBuilder::new()
-        .title(title)
-        .content(Html(text))
-        .size(800, 600)
-        .debug(debug)
-        .resizable(true)
-        .user_data(())
-        .invoke_handler(|_, _| Ok(()))
-        .run()
-        .unwrap();
+    let view = WebView::new(
+        title,
+        Content::Html(text),
+        800,
+        600,
+        true,
+        debug,
+    ).unwrap();
+
+    view.join();
 }
